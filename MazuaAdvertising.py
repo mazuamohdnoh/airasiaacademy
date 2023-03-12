8eea5eecdad097d63931eb357bc3b9c5c61deefb
mazuamohdnoh <127641661+mazuamohdnoh@users.noreply.github.com>
2023-03-12 15:34:30 +0800

    Create MazuaAdvertising.py

---
+++ MazuaAdvertising.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+st.write("""
+# MazuaAdvertising Prediction App
+
+This app predicts the **Sales** by media platform!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
+    Radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
+    Newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+loaded_model = pickle.load(open("MazuaAdvertising.h5", "rb"))
+
+prediction = loaded_model.predict(df)
+
+st.subheader('Prediction')
+st.write(prediction)
